22c66f5e553d4db435432a791b4dd02d61fdb87d
Unisha <unishakhanal3@gmail.com>
2026-06-30 22:22:27 +0545

    Update project

--- next.config.ts
+++ next.config.ts
@@ -5,14 +5,6 @@ const nextConfig: NextConfig = {
   compiler: {
     styledJsx: true,
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:5000/api/:path*',
-      },
-    ];
-  },
 };
 
 export default nextConfig;
